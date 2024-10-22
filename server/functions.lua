2021d1ef36565c96e97f754dfa2b35973f90b5c8
Esteban <87365724+TheLion051@users.noreply.github.com>
2024-10-21 23:32:05 -0600

    ESX version

--- server/functions.lua
+++ server/functions.lua
@@ -1,29 +1,35 @@
-local QBCore = exports['qb-core']:GetCoreObject()
+ESX = exports["es_extended"]:getSharedObject()
 
 function ObtainDiscordIdentifier(src)
-    local discord = QBCore.Functions.GetIdentifier(src, 'discord')
-    return string.sub(discord, 9)
+    local identifiers = GetPlayerIdentifiers(src)
+    for _, identifier in ipairs(identifiers) do
+        if string.find(identifier, "discord:") then
+            return string.sub(identifier, 9)
+        end
+    end
+    return nil
 end
 
 function Notify(src, text, notifType)
-    QBCore.Functions.Notify(src, text, notifType)
+    local xPlayer = ESX.GetPlayerFromId(src)
+    xPlayer.showNotification(text)
 end
 
 function ProduceReward(src)
-    local Player = QBCore.Functions.GetPlayer(src)
-    if not Player then return false end
+    local xPlayer = ESX.GetPlayerFromId(src)
+    if not xPlayer then return false end
 
     if Config.Reward.type == 'bank' then
-        Player.Functions.AddMoney('bank', Config.Reward.amount, 'Discord Booster Reward')
+        xPlayer.addAccountMoney('bank', Config.Reward.amount)
         return true
-    elseif Config.Reward.type == 'cash' then
-        Player.Functions.AddMoney('cash', Config.Reward.amount, 'Discord Booster Reward')
+    elseif Config.Reward.type == 'money' then
+        xPlayer.addMoney(Config.Reward.amount)
         return true
     elseif Config.Reward.type == 'item' then
-        Player.Functions.AddItem(Config.Reward.item, Config.Reward.amount, nil, {})
+        xPlayer.addInventoryItem(Config.Reward.item, Config.Reward.amount)
         return true
     end
 
-    print('b-discordbooster | INVALID REWARD TYPE')
+    print('esx-discordbooster | INVALID REWARD TYPE')
     return false
 end
